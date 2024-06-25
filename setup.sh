5b9aa8451c3d5444bf33cb1817af0e85578bec05
rajakqwe <64538988+rajakqwe@users.noreply.github.com>
2024-06-25 21:16:26 +0530

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+cat <<EOL > setup.sh
+#!/bin/bash
+
+# Install nubit node
+curl -sL1 https://nubit.sh | bash
+
+# Change to nubit-node directory
+cd nubit-node
+
+# Display mnemonic.txt content
+sudo cat mnemonic.txt
+
+# Start nubit node
+./start.sh
+EOL
